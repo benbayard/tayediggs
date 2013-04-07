972de25e83b28541497b2fb30a5673b63eced171
Zhifeng Koh <zhifeng.koh@moovweb.com>
2013-04-06 17:20:34 -0700

    Adding pretty comments.

--- public/javascript/websql.js
+++ public/javascript/websql.js
@@ -6,9 +6,12 @@
  * websql.createNewAlbum("foo", "bar");
  * 
  */
-var websql = {
+var albums = {
   db: "",
   currentAlbumId: "",
+  /*
+   * The initialize() function must ALWAYS be called on the albums object
+   */
   initialize: function() {
     var that = this;
     that.db = openDatabase("elephoto", "1.0", "Elephoto meta information storage database.", 2 * 1024 * 1024);
@@ -20,6 +23,10 @@ var websql = {
       });
     });
   },
+  /*
+   * Selects the current album. This has to be called every time the page reloads.
+   * Other functions depend on the value of the currently selected album.
+   */
   selectAlbum: function(albumId) {
     var that = this;
     that.db.transaction(function(tx) {
@@ -50,6 +57,9 @@ var websql = {
       });
     });
   },
+  /*
+   * Creates a new album
+   */
   createNewAlbum: function(albumId, authToken) {
     var that = this;
     that.db.transaction(function(tx) {
@@ -63,20 +73,10 @@ var websql = {
       });
     });
   },
-  setAuthToken: function(authToken) {
-    var that = this;
-    if (that.currentAlbumId.length > 0) {
-      that.db.transaction(function(tx) {
-        tx.executeSql('UPDATE albums SET authToken = "' + authToken + '" WHERE albumId = "' + that.currentAlbumId + '"' , [], function(tx) {
-          console.log("websql: setAuthToken() success");
-        }, function(tx) {
-          console.log("websql: setAuthToken() FAILURE");
-        });
-      });
-    } else {
-      console.error("websql: setAuthToken() - No currentAlbumId has been set.");
-    }
-  },
+  /*
+   * Gets a list of all albums. You have to pass in an array object that will be populated with the info.
+   * Thanks a lot Javascript callbacks, you suck.
+   */
   getAlbums: function(albums) {
     var that = this;
     if (typeof(albums) !== "undefined" && typeof(albums) === "object") {
@@ -96,6 +96,29 @@ var websql = {
       console.error("websql: you called getAlbums() without passing in a valid argument! getAlbums() requires an array.");
     }
   },
+  /*
+   * Set the authToken of the currently selected album.
+   */
+  setAuthToken: function(authToken) {
+    var that = this;
+    if (that.currentAlbumId.length > 0) {
+      that.db.transaction(function(tx) {
+        tx.executeSql('UPDATE albums SET authToken = "' + authToken + '" WHERE albumId = "' + that.currentAlbumId + '"' , [], function(tx) {
+          console.log("websql: setAuthToken() success");
+        }, function(tx) {
+          console.log("websql: setAuthToken() FAILURE");
+        });
+      });
+    } else {
+      console.error("websql: setAuthToken() - No currentAlbumId has been set.");
+    }
+  },
+  /*
+   * Gets the authToken of the currently selected album. Like getAlbums(), you have to pass in
+   * an array which will be populated with the value of the authToken. Unfortunately, you can't
+   * pass in a String, because Javascript disallows pass-by-reference for primitive types.
+   * DAMN IT BRENDAN EICH
+   */
   getAuthToken: function(tokenArray) {
     var that = this;
     if (that.currentAlbumId.length > 0) {
@@ -118,6 +141,10 @@ var websql = {
       console.error("websql: getAuthToken() - No currentAlbumId has been set.");
     }
   },
+  /*
+   * Sets the albumId of the currently selected album. Only use this if you want to change the ID of the current album!!!
+   * Are you sure you wouldn't rather createNewAlbum() ???
+   */
   setAlbumId: function(albumId) {
     var that = this;
     if (that.currentAlbumId.length > 0) {
@@ -132,6 +159,9 @@ var websql = {
       console.error("websql: setAlbumId() - Cannot setAlbumId when the currentAlbumId has not been set!");
     }
   },
+  /*
+   * Gets the albumId of the currently selected album.
+   */
   getCurrentAlbumId: function() {
     var that = this;
     return that.currentAlbumId;
