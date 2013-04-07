a41cc8aec8be61f0ffee872d382bbf8c3c69f8af
Matt Mo <matt.mo@moovweb.com>
2013-04-06 17:39:23 -0700

    Updated foursquare.js, added getVenues method

--- public/javascript/foursquare.js
+++ public/javascript/foursquare.js
@@ -1,7 +1,13 @@
+function venue(name, address) {
+  this.name = name;
+  this.address = address;
+}
+
 var Foursquare = {
   // Venue Response
   
-  venueResponse: "",
+  venueResponse: null,
+  
   setup: function () {
     
   },
@@ -14,16 +20,43 @@ var Foursquare = {
       + "&oauth_token=" + mattTempAccessToken 
       + "&v=" + oauthversion,
       type: "GET",
+      dataType: 'json'
     }).success(function(data) {
-      console.log(data)
+      console.log("https://api.foursquare.com/v2/venues/search?ll=" + coords 
+      + "&oauth_token=" + mattTempAccessToken 
+      + "&v=" + oauthversion)
+      Foursquare.venueResponse = data.response;
     }).error(function() {
       alert('setVenueResponse: FAIL!!!! damn you foursquare!');
     });
+  },
+  
+  // Return an array of venue objects (name and address of business)
+  
+  // Location object:
+  //   address: "725 Littlefield Ave"
+  //   cc: "US"
+  //   city: "South San Francisco"
+  //   country: "United States"
+  //   distance: 234
+  //   lat: 37.698091
+  //   lng: -122.398873
+  //   state: "CA"
+  
+  getVenues: function() {
+    if (Foursquare.venueResponse !== null) {
+      var venues = [];
+      var i = 0;
+      Foursquare.venueResponse.venues.forEach( function(el) {
+        venues[i] = new venue(el.name, el.location);
+      });
+    }
   }
   
-  
 }
+
 $(document).ready(function() {
-  var test_coords = "51.505,-0.09"
-  // Foursquare.setup();
+  var test_coords = "51.505,-0.09";
+  Foursquare.setVenueResponse(test_coords);
+  Foursquare.getAddresses();
 });
